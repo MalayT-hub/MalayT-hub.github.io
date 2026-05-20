fd072a22b7ccebd4bf28b6ea8ce28c58004d6a87
MalayT-hub <malay.thomas@inspire35.org>
2026-05-20 18:34:41 +0000

    finished

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -53,8 +53,8 @@ $(document).on("keyup", handleKeyUp);
   */
   function newFrame() {
     repositionGameItem();
-console.log(walker.x, walker.y);
-redrawGameItem()
+    wallCollision();
+    redrawGameItem()
   }
  
   /*
@@ -131,7 +131,20 @@ else{}
 
  
   function wallCollision() {
-   
+    if(walker.x < 0 ){
+      walker.x -= walker.speedX;
+    }
+    if(walker.y < 0 ){
+      walker.y -= walker.speedY;
+    }
+     if(walker.x > $("#board").width()-50){
+      walker.x -= walker.speedX;
+    }
+     if(walker.y > $("#board").height()-50){
+      walker.y -= walker.speedY;
+    }
+    // stop the interval timer
+    //clearInterval(interval);
   }
 }
 
